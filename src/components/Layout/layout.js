2bee9590e7368ea635fd9159ac3355ede325b851
LeoMrel <leonardomrel0028@gmail.com>
2021-11-30 18:25:13 -0300

    added custom favicon

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children, bgColor, height, id, padding, notFound }) => {
       id={id}
       className={`${height} ${bgColor} ${padding} place-items-center flex transform transition-all duration-1000`}
     >
-      <Seo title={notFound || "Leo"} />
+      <Seo title={notFound || "eo"} />
       <div className="w-full flex place-content-center">
         <div className="w-full max-w-5xl text-5xl md:text-6xl px-3 font-montserrat flex text-gray-800 overflow-hidden">
           {children}
